92036e1f38d764f0299e46fb6409036073be8e24
PyRoh <117882176+PythonRoh@users.noreply.github.com>
2025-07-15 00:23:17 +0530

    user and video DBmodels added, hooks, bcrypt and JWT

---
+++ src/models/user.model.js
@@ -0,0 +1,104 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hash the password before saving it to the database
+// using the pre hook middleware and bcrypt library
+// encryption is done in the pre save hook to ensure that the password is never stored as plain text.
+
+// REMEBER IN these cases to use function keyword instead of arrow function, becoz,
+// 'this' keyword inside a pre hook function refers to the document being saved
+// 'this' keyword is not available in arrow functions.
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Check if the password is correct when a user tries to login
+userSchema.methods.isPasswordCorrect = async function (password) {
+  // bcrypt.compare(plaintext, hashed) is a method that compares the plaintext password with the hashed password.
+  return await bcrypt.compare(password, this.password);
+};
+
+
+// Generate an access token for a user
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// Generate a refresh token for a user
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
